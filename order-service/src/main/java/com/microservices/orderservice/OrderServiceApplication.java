5fb5206349a0f491d3bb788f4d2708f2dd0d51f1
naveed008 <naveedhakim456@gmail.com>
2024-02-16 00:33:41 +0500

    modified helloworld 123 in OrderServiceApplication

--- order-service/src/main/java/com/microservices/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/microservices/orderservice/OrderServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("hello world123");
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
